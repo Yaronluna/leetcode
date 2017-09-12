2d79d6ce2407fdaf37f8cd1eed81dd1475d6ccf7
Nan Sun <nan.sun@hudsonltd.com>
2017-09-12 18:45:39 -0400

    50. Pow(x, n), Implement pow(x, n).

---
+++ javascript/50. Pow(x, n).js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+/*
+for eample
+    N = 9 = 2^3 + 2^0 = 1001 in binary. Then:
+    x^9 = x^(2^3) * x^(2^0)
+*/
+var myPow = function(x, n) {    
+  return (n < 0) ? ( 1 / pow(x, n)) : pow(x, n);
+};
+
+function pow(x, n) {
+  if (n == 0) {
+      return 1;
+  }
+  
+  var v = pow(x, parseInt(n/2));
+  
+  if (n % 2 === 0) {
+      return v * v;
+  }
+  
+  return v * v * x;
+}
